f15d73bc5866c8bd45a14de4e3a2b4f66afe41e4
Kathirone <105572499+Kathirone@users.noreply.github.com>
2022-06-12 07:52:31 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,10 +10,10 @@ public class App
 
     public App() {}
 
- /*   public static void main(String[] args) {
+   public static void main(String[] args) {
         System.out.println(new App().getMessage());
     }
-*/
+    
     private final String getMessage() {
         return message;
     }
